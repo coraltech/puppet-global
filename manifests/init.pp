ba65746c99fd7e04405726abdc595f76cbebb236
Adrian Webb <adrian.webb@venturiscm.com>
2012-05-22 23:46:32 -0400

    Cleaning up the header comments and adding a sanity check around the build_essential package.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,33 +1,36 @@
-#
 # Class: global_lib
 #
-# This module installs misc global packages and utilities that do not fit neatly
-# into specialized bundles.
+#   This module installs misc packages and utilities that do not fit
+#   neatly into specialized bundles.
+#
+#   Adrian Webb <adrian.webb@coraltg.com>
+#   2012-05-22
+#
+#   Tested platforms:
+#    - Ubuntu 12.04
 #
 # Parameters:
 #
-# None
-#
 # Actions:
 #
-# 1. Installs global general purpose packages on the server.
+#   Installs general purpose packages on the server.
 #
 # Requires:
 #
-# None
-#
 # Sample Usage:
 #
-# include global_lib
+#   include global_lib
 #
+# [Remember: No empty lines between comments and class definition]
 class global_lib {
 
   include global_lib::params
 
   #-----------------------------------------------------------------------------
 
-  package {
-    'build-essential':
+  if $global_lib::params::build_essential_version {
+    package { 'build-essential':
       ensure => $global_lib::params::build_essential_version;
+    }
   }
 }
