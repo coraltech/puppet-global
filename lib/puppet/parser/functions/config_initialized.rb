bf1cd227f3613c53731c1e63ddfdda4dbe24bfe3
Adrian Webb <adrian.webb@venturiscm.com>
2012-12-28 16:42:53 -0500

    Abstracting hiera_is_available function to config_initialized and fixing issue with hiera_ready fact.

--- lib/puppet/parser/functions/config_initialized.rb
+++ lib/puppet/parser/functions/config_initialized.rb
@@ -1,16 +1,18 @@
 #
-# hiera_is_available.rb
+# config_initialized.rb
 #
 # This function checks if Hiera is fully configured and ready to query.
 #
 module Puppet::Parser::Functions
-  newfunction(:hiera_is_available, :type => :rvalue, :doc => <<-EOS
+  newfunction(:config_initialized, :type => :rvalue, :doc => <<-EOS
 This function checks if Hiera is fully configured and ready to query.
     EOS
 ) do ||
 
+    Puppet::Parser::Functions.autoloader.loadall
+
     begin
-      if Puppet::Parser::Functions.function('hiera') && lookupvar("::hiera_ready") == true
+      if Puppet::Parser::Functions.function('hiera') && lookupvar("::hiera_ready") == 'true'
         return true
       end
     
